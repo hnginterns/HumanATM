ed49dc1a7a0a6741c7b01f502246ba73ffd7347d
Mofope Ojosh <mofopet@gmail.com>
2017-10-30 00:41:19 +0100

    Few changes in layout

--- humanAtmApp/resources/views/layouts/app.blade.php
+++ humanAtmApp/resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-    <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -16,6 +15,7 @@
     @yield('title')
 
     <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/header-footer.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
--- humanAtmApp/resources/views/locationlisting.blade.php
+++ humanAtmApp/resources/views/locationlisting.blade.php
@@ -6,100 +6,106 @@
 @endsection
 
 @section('content')
-
-<body>
-    <div class="container-fluid daily-margin">
-        <!-- header -->
-        @include('header')
-        <div class="row profile-rower">
-            <div class="col-md-8 col-md-offset-2 profile-borderColor">
-                <div class="row">
-                    <div class="col-md-12 text-right profile-boxColour">
-                        <h4>Make a request</h4>
-                    </div>
-                    <div>
-
+    
+       <div class="container-fluid daily-margin">
+            <!-- header -->
+            <div class="row profile-rower">
+                    <div class="col-md-8 col-md-offset-2 profile-borderColor">
                         <div class="row">
-                            <div class="col-md-4 col-md-offset-3 profile-boxColour text-center">
-                                <h4> HUMAN ATM</h4>
+                            <div class="col-md-12 text-right profile-boxColour">
+                                <h4>Make a request</h4>
                             </div>
-                        </div>
+                        <div>
 
-                        <div class="row align-items-center profile-margin">
-                            <div class="col-md-7 col-md-offset-1 text-center profile-box2">
-                                <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
-                            </div>
-
-                            <div class="col-md-2 col-md-offset-1 text-center ">
-                                <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
-                            </div>
-                        </div>
-
-                        <div class="row align-items-center profile-margin">
-                            <div class="col-md-7 col-md-offset-1 text-center profile-box2">
-                                <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                            <div class="row">
+                                <div class="col-md-4 col-md-offset-3 profile-boxColour text-center">
+                                    <h4> HUMAN ATM</h4>
+                                </div>
                             </div>
 
-                            <div class="col-md-2 col-md-offset-1 text-center ">
-                                <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                            <div class="row align-items-center profile-margin">
+                                <div class="col-md-7 col-md-offset-1 text-center profile-box2">
+                                    <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                                </div>
+
+                                <div class="col-md-2 col-md-offset-1 text-center ">
+                                    <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
                             </div>
-                        </div>
-                        <div class="row align-items-center profile-margin">
-                            <div class="col-md-7 col-md-offset-1 text-center profile-box2">
-                                <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
                             </div>
 
-                            <div class="col-md-2 col-md-offset-1 text-center ">
-                                <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
-                            </div>
-                        </div>
-                        <div class="row align-items-center profile-margin">
-                            <div class="col-md-7 col-md-offset-1 text-center profile-box2">
-                                <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
-                            </div>
+                            <div class="row align-items-center profile-margin">
+                                <div class="col-md-7 col-md-offset-1 text-center profile-box2">
+                                    <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                                </div>
 
-                            <div class="col-md-2 col-md-offset-1 text-center ">
-                                <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                                <div class="col-md-2 col-md-offset-1 text-center ">
+                                        <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row align-items-center profile-margin">
-                            <div class="col-md-7 col-md-offset-1 text-center profile-box2">
-                                <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
-                            </div>
+                            <div class="row align-items-center profile-margin">
+                                <div class="col-md-7 col-md-offset-1 text-center profile-box2">
+                                    <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                                </div>
 
-                            <div class="col-md-2 col-md-offset-1 text-center ">
-                                <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                                <div class="col-md-2 col-md-offset-1 text-center ">
+                                        <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                                </div>
                             </div>
-                        </div>
+                            <div class="row align-items-center profile-margin">
+                                <div class="col-md-7 col-md-offset-1 text-center profile-box2">
+                                    <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                                </div>
 
+                                <div class="col-md-2 col-md-offset-1 text-center ">
+                                        <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                                </div>
+                            </div>
+                            <div class="row align-items-center profile-margin">
+                                <div class="col-md-7 col-md-offset-1 text-center profile-box2">
+                                    <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                                </div>
 
-                        <div class="row ">
-                            <div class="col-md-4 col-md-offset-3 profile-boxColour profile-static text-center">
-                                <h4> STATIC ATM</h4>
+                                <div class="col-md-2 col-md-offset-1 text-center ">
+                                        <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                                </div>
                             </div>
-                        </div>
+                          
 
-                        <div class="row align-items-center profile-margin">
-                            <div class="col-md-7 col-md-offset-1 text-center profile-box2">
-                                <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                                <div class="row ">
+                                    <div class="col-md-4 col-md-offset-3 profile-boxColour profile-static text-center">
+                                        <h4> STATIC ATM</h4>
+                                    </div>
+                                </div>
+    
+                                <div class="row align-items-center profile-margin">
+                                    <div class="col-md-7 col-md-offset-1 text-center profile-box2">
+                                        <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                                    </div>
+    
+                                    <div class="col-md-2 col-md-offset-1 text-center ">
+                                        <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                                </div>
+                                </div>
+                                <div class="row align-items-center profile-margin">
+                                    <div class="col-md-7 col-md-offset-1 text-center profile-box2">
+                                        <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                                    </div>
+    
+                                    <div class="col-md-2 col-md-offset-1 text-center ">
+                                        <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                                </div>
+                                </div>
+                                <div class="row align-items-center profile-margin">
+                                    <div class="col-md-7 col-md-offset-1 text-center profile-box2">
+                                        <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
+                                    </div>
+    
+                                    <div class="col-md-2 col-md-offset-1 text-center ">
+                                        <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                                </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row align-items-center profile-margin">
-                            <div class="col-md-7 col-md-offset-1 text-center profile-box2">
-                                <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
-                            </div>
-
-                            <div class="col-md-2 col-md-offset-1 text-center ">
-                                <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
-                            </div>
-                        </div>
-                        <div class="row align-items-center profile-margin">
-                            <div class="col-md-7 col-md-offset-1 text-center profile-box2">
-                                <h4>Elisha Nseobong Ukpong - Uyo, Akwa Ibom State</h4>
-                            </div>
-
-                            <div class="col-md-2 col-md-offset-1 text-center ">
-                                <button type="button" class="btn btn-primary btn-block profile-box3"> REQUEST </button>
+                            
                             </div>
                         </div>
                     </div>
@@ -107,8 +113,4 @@
                 </div>
             </div>
         </div>
-
-    </div>
-</div>
-</div>
 @endsection
