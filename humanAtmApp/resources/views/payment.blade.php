a5510dc0fd7288a493388f73515b59d9dfee1546
Mofope Ojosh <mofopet@gmail.com>
2017-10-29 15:23:49 +0100

    Added wallet, payment and withdraw blades

---
+++ humanAtmApp/resources/views/payment.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+    @section('title')
+        <title>Payment | HumanATM</title>
+    @endsection
+
+    @section('content')
+            <section class="container">
+                <div class="profile-container">
+                        <div class="withdraw-content">
+                                <div class="profile-request">
+                                    <p>MAKE A REQUEST</p>
+                                </div>
+                            <div class="profile-supermenu">
+                                <div class ="profile-menu" id="">
+                                    <button type="button">PROFILE</button>
+                                    <button type="button" >WITHDRAW</button>
+                                    <button type="button" class="active">PAYMENT</button>
+                                </div>
+                            </div>
+                                
+                            <div class="withdraw-info">
+                                <form action="" class="profile-innerform">
+                                    <div class="form-group">
+                                        <label for="phone number">PHONE NUMBER</label>
+                                        <input type="text" name="payment-number" class="withdraw-data" id="" placeholder="08022343254">
+                                        
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="amount">AMOUNT</label>
+                                        <input type="text" name="payment-amount" class="withdraw-data" id="" placeholder="N45000">
+                                        <p class="withdraw-label">MAX. AMOUNT  N50,000</p>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="acct number">ACCOUNT NUMBER</label>
+                                        <input type="text" name="payment-acct" class="withdraw-data" id="" placeholder="6160883639">
+                                        <p class="withdraw-label">OLIBIE CHIDERA</p>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="location">BANK</label>
+                                        <input type="text" name="payment-location" id="" class="withdraw-data" placeholder="GUARANTY TRUST BANK">
+                                    </div>
+                                    <div class="">              
+                                        <p id='payment-delivery'>
+                                            <span>Transaction-fee</span>
+                                            <span id="payment-span1">N55</span>
+                                        </p>
+                                        <input type="submit" class="withdraw-data withdraw-submit" id="payment-submit"value="SEND">
+                                        <input type="submit" class="withdraw-data withdraw-submit" id="withdraw-submit1"value="FUND WALLET">
+                                    </div>  
+                                </form>
+                            </div>
+                        </div>
+        
+                </div>       
+                
+            </section>
+@endsection
---
+++ humanAtmApp/resources/views/wallettoaccount2.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+    @section('title')
+        <title> Wallet | HumanATM</title>
+    @endsection
+
+    @section('content')
+            <div class="container-fluid daily-margin">
+                <div class="row profile-rower">
+                        <div class="col-md-8 col-md-offset-2 profile-borderColor">
+                            <div class="row">
+                                <div class="col-md-12 wallet-boxColour">
+                                    <b><p>Wallet Balance</p></b>
+                                    <h1><b>N140,000</b></h1>
+                                </div>
+                            </div>
+                            
+                        <div class="row daily-margin text-center">
+                            <div class="col-md-4 col-md-offset-1 daily-inactive">
+                                <h4><a href="wallettoaccount.php"> <span style="color:#0F3B75"><b> TRANSFER TO ACCOUNT</b><span></a></h4>
+                            </div>
+                            <div class="col-md-4 col-md-offset-2 daily-active">
+                                <h4><a href="wallettoaccount2.php"><span style="color:white "><b>FUND WALLET</b><span></a></h4>
+                            </div>
+                        </div><br>
+                        <div class="row text-center daily-margin">
+                                <div class="col-md-12">
+                                    <img src="img/4.png" alt="">
+                                </div>
+                            </div>
+                        <div class="row daily-margin">
+                            <div class="col-md-10 col-md-offset-1">
+
+                                    <form action="#" class="form" method="get">
+                                            <div class="form-group ">
+                                                <label for="Amount" class="walletColor">AMOUNT</label>
+                                                <input type="text" class="form-control wallet-input" placeholder="dikethelmak">
+                                                <p class="walletLabel-below">MAX. AMOUNT  N50,000</p>
+                                           
+                                                <label for="Amount" class="walletColor">ACCOUNT NUMBER</label>
+                                                <input type="text" class="form-control wallet-input" placeholder="0334892134">
+                                                <p class="walletLabel-below">DIKE THELMA KELECHI</p>
+                                           
+                                        
+                                                <label for="Amount" class="walletColor">BANK</label>
+                                                <input type="text" class="form-control wallet-input" placeholder="First Bank">
+                                           			<br><br>
+                                                <button type="submit" class=" wallet-button">PAY N301,000</button>
+                                            </div>
+                                        </form>
+                            </div>
+                        </div>
+                        </div>
+                </div>
+            </div>
+@endsection
