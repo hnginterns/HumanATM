c03a711271ffbbeae5596436cd6d7b77be52b38e
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-10-29 22:46:59 +0100

    add 404

---
+++ frontend/404.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title')
+<title> Payment | HumanATM</title>
+@endsection
+
+@section('content')
+<body>
+	<div class="container-fluid">
+		<!-- header -->
+		@include('header')
+		<div class="row">
+			<div class="col-sm-offset-3 col-sm-6  page404-rectangle-box" >
+				<div class="page404-main text-center">
+					<h2 class="page404-header"><b>ERROR</b></h2>
+					<img src="img/Group.png" class="img-responsive " height="150px">
+					<p class="page404-paragraph"> Oops!! i think you're on the wrong track to meeting the Human Atm 
+						<br>of your choice, you can go to the homepage to restart your journey. </p>
+
+						<div class="page404-navigate">
+							<a class="page404-bg-blue" href="#">Go back to Homepage</a><br><br>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	@endsection
