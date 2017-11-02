f3aca011ccb9618a438cdae8fd17c060038db672
drumzminister <ishukpong418@gmail.com>
2017-11-02 22:54:07 +0100

    updated home page

--- humanAtmApp/resources/views/footer.blade.php
+++ humanAtmApp/resources/views/footer.blade.php
@@ -1,4 +1,51 @@
-<div class="footer">
-        <p class="footer-block"><strong>Copyright &copy; 2017 Human ATM.</strong> All rights reserved.</p>
-    </div>
-</div>
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
+    <div classs="row">
+        <div class="all-footer footer-head">
+            <!-- address -->
+            <div class="col-sm-2  col-sm-offset-2 text-center">
+                <h5><span>Address</span></h5>
+                    <hr>
+                <p class="footer-p"> Plot 34, Unit 23 e-Library Estate, Akwa Ibom State, Nigeria</p>
+            
+            </div>
+            <!-- email -->
+            <div class="col-sm-2 col-sm-offset-1 text-center">
+                <h5><span>Email</span></h5>
+                    <hr>
+                    <p class="footer-p">contactus@humanatm.com</p>
+                    <p class="footer-p">headoffice@humanatm.com</p>
+            </div>
+            <!--  phone-->
+            <div class="col-sm-2 col-sm-offset-1 text-center">
+                <h5><span>Phone</span></h5>
+                <hr>
+                    <p class="footer-p"> +234(0)8987767877</p>
+                    <p class="footer-p">+234(0)8035502146</p>
+            </div>
+      <!-- copyright -->
+        <div class="row">
+            <div class="col-sm-12 text-center">
+             <p class="footer-end"><strong>Copyright &copy; 2017 Human ATM.</strong> <br>All rights reserved.</p>
+        
+            </div>
+        <div>
+
+
+
+
+
