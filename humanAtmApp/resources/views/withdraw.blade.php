81a8bf176870a49501e098ffc2e4ca902906f135
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-11-02 11:14:34 +0100

    added bank select to withdraw blade

--- humanAtmApp/resources/views/withdraw.blade.php
+++ humanAtmApp/resources/views/withdraw.blade.php
@@ -7,9 +7,9 @@
 @section('content')
 <body class="signup-body">
     <section>
-       @include('header')
-   </section>
-   <section class="container">
+     @include('header')
+ </section>
+ <section class="container">
     <div class="profile-container">
         <div class="withdraw-content">
             <div class="profile-request">
@@ -27,8 +27,8 @@
 
             <div class="withdraw-info">
                 <form action="/withdraw/{{$withdrawer_id}}" class="profile-innerform" method="POST">
-                   {{ csrf_field() }}
-                   <div class="form-group{{ $errors->has('phone_number') ? ' has-error' : '' }}">
+                 {{ csrf_field() }}
+                 <div class="form-group{{ $errors->has('phone_number') ? ' has-error' : '' }}">
                     <label for="phone number">PHONE NUMBER</label>
                     <input type="text" name="phone_number" class="withdraw-data" id="" placeholder="08022343254" value="{{old('phone_number')}}">
                     @if ($errors->has('phone_number'))
@@ -42,18 +42,40 @@
                     <label for="amount">AMOUNT</label>
                     <input type="text" name="amount" class="withdraw-data" id="" placeholder="45000" value="{{ old('amount')}}">
                     <p class="withdraw-label">MAX. AMOUNT  N50,000</p>
-                     @if ($errors->has('amount'))
+                    @if ($errors->has('amount'))
                     <span class="help-block">
                         <strong>{{ $errors->first('amount') }}</strong>
                     </span>
                     @endif
                 </div>
 
+                 <div class="form-group{{ $errors->has('bank_name') ? ' has-error' : '' }}">
+                <label for="Amount" class="walletColor">BANK NAME</label>
+                <select class="withdraw-expand" name="bank_name">
+                    <option value="" selected="selected">Select Your Bank</option>
+                    <option value="First Bank of Nigeria" >First Bank of Nigeria</option>
+                    <option value="Union Bank of Nigeria">Union Bank of Nigeria</option>
+                    <option value="United Bank for Africa">United Bank for Africa</option>
+                    <option value="Unity Bank plc">Unity Bank plc</option>
+                    <option value="fidelity Bank Nigeria">Fidelity Bank Nigeria</option>
+                    <option value="First City Monument Bank">First City Monument Bank</option>
+                    <option value="Guaranty Trust_Bank">Guaranty Trust Bank</option>
+                    <option value="Heritage Bank Plc">Heritage Bank plc</option>
+                </select>
+                 @if ($errors->has('bank_name'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('bank_name') }}</strong>
+                    </span>
+                    @endif
+                </div>
+
+
+
                 <div class="form-group{{ $errors->has('account_number') ? ' has-error' : '' }}">
                     <label for="acct number">ACCOUNT NUMBER</label>
                     <input type="text" name="account_number" class="withdraw-data" id="" placeholder="6160883639" value="{{ old('account_number')}}">
                     <p class="withdraw-label">OLIBIE CHIDERA</p>
-                     @if ($errors->has('account_number'))
+                    @if ($errors->has('account_number'))
                     <span class="help-block">
                         <strong>{{ $errors->first('account_number') }}</strong>
                     </span>
@@ -63,7 +85,7 @@
                 <div class="form-group{{ $errors->has('location') ? ' has-error' : '' }}">
                     <label for="location">LOCATION</label>
                     <input type="text" name="location" id="" class="withdraw-data" placeholder="NO 3 BARREL STRT YABA" value="{{ old('location')}}">
-                     @if ($errors->has('location'))
+                    @if ($errors->has('location'))
                     <span class="help-block" >
                         <strong>{{ $errors->first('location') }}</strong>
                     </span>
