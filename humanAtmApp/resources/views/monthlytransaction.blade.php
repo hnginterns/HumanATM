a906ae3a40f4586d223bc01302619d2d54f529db
drumzminister <ishukpong418@gmail.com>
2017-11-03 03:12:29 +0100

    updated transactions

--- humanAtmApp/resources/views/monthlytransaction.blade.php
+++ humanAtmApp/resources/views/monthlytransaction.blade.php
@@ -5,105 +5,106 @@
     @endsection
 
     @section('content')
-
-        <div class="container-fluid daily-margin">
+    <body>
+        <div class="container-fluid blue-fall outerBox-margin">
         @include('header')
-            <div class="row profile-rower">
-                    <div class="col-md-8 col-md-offset-2 profile-borderColor">
-                        <div class="row">
-                            <div class="col-md-12 text-right profile-boxColour">
-                                <h4>Transactions</h4>
-                            </div>
+        <div class="row box-margin">
+                <div class="col-md-8 col-md-offset-2 box-border">
+                    <div class="row">
+                        <div class="col-md-12 text-center box-color">
+                            <h3>TRANSACTION</h3><hr>
                         </div>
-                        <div class="row paybills-margin2">
-                            <div class="col-md-2 col-md-offset-2 text-center daily-inactive">
-                                <h4><a href="dailytransaction"><span style="color:#0F3B75;"><strong>Today</strong></span></a></h4>
-                                   
-                            </div>
-                            <div class="col-md-2 col-md-offset-1 text-center daily-inactive">
-                                <h4><a href="weeklytransaction"><span style="color:#0F3B75;"><strong>Week</strong></span></a></h4>
-                                   
-                            </div>
-                            <div class="col-md-2 col-md-offset-1 text-center daily-active">
-                                <h4><a href="monthlytransaction"><span style="color:white"><strong>Month</strong></span></a></h4>
-                                   
-                            </div>
-                            
+                    </div>
+                    <div class="row paybills-margin2">
+                        <div class="col-md-2 col-md-offset-3 text-center ">
+                        <a href="dailytransaction"><h4 class="box-inactive">Today</h4></a>
+                                
                         </div>
-                       
-                        <div class="row paybills-margin2">
-                            <div class="col-md-10 col-md-offset-1 daily-borderColor">
-                                <div class="row">
-                                    <div class="col-md-12 text-right">
-                                        <h4><strong>Paid</strong></h4>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 daily-margin">
-                                        <h3><strong>N30,000</strong></h3>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-right daily-margin">
-                                        <p><strong>Wallet ID: chykeofficial</strong></p>
-                                    </div>
+                        <div class="col-md-2 col-md-offset-0 text-center">
+                        <a href="weeklytransaction"><h4 class="box-inactive">Week</h4></a>
+                                
+                        </div>
+                        <div class="col-md-2 col-md-offset-0 text-center">
+                            <a href="monthlytransaction"><h4 class="box-active">Month</h4></a>                                
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="row paybills-margin2">
+                        <div class="col-md-8 col-md-offset-2 daily-borderColor">
+                            <div class="row">
+                                <div class="col-md-12 text-center">
+                                    <h4><strong>Paid</strong></h4>
                                 </div>
                             </div>
-                            <div class="col-md-10 col-md-offset-1 daily-borderColor">
-                                <div class="row">
-                                    <div class="col-md-12 text-right">
-                                        <h4><strong>Paid</strong></h4>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 daily-margin">
-                                        <h3><strong>N30,000</strong></h3>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-right daily-margin">
-                                        <p><strong>Wallet ID: chykeofficial</strong></p>
-                                    </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center daily-margin">
+                                    <h1><strong>N30,000</strong></h1>
                                 </div>
                             </div>
-                            <div class="col-md-10 col-md-offset-1 daily-borderColor">
-                                <div class="row">
-                                    <div class="col-md-12 text-right">
-                                        <h4><strong>Paid</strong></h4>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 daily-margin">
-                                        <h3><strong>N30,000</strong></h3>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-right daily-margin">
-                                        <p><strong>Wallet ID: chykeofficial</strong></p>
-                                    </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center daily-margin">
+                                    <h4><strong>Wallet ID: chykeofficial</strong></h4>
                                 </div>
                             </div>
-                            <div class="col-md-10 col-md-offset-1 daily-borderColor">
-                                <div class="row">
-                                    <div class="col-md-12 text-right">
-                                        <h4><strong>Paid</strong></h4>
-                                    </div>
+                        </div><div class="col-md-8 col-md-offset-2 daily-borderColor">
+                            <div class="row">
+                                <div class="col-md-12 text-center">
+                                    <h4><strong>Paid</strong></h4>
                                 </div>
-                                <div class="row">
-                                    <div class="col-md-12 daily-margin">
-                                        <h3><strong>N30,000</strong></h3>
-                                    </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center daily-margin">
+                                    <h1><strong>N30,000</strong></h1>
                                 </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-right daily-margin">
-                                        <p><strong>Wallet ID: chykeofficial</strong></p>
-                                    </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center daily-margin">
+                                    <h4><strong>Wallet ID: chykeofficial</strong></h4>
+                                </div>
+                            </div>
+                        </div><div class="col-md-8 col-md-offset-2 daily-borderColor">
+                            <div class="row">
+                                <div class="col-md-12 text-center">
+                                    <h4><strong>Paid</strong></h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center daily-margin">
+                                    <h1><strong>N30,000</strong></h1>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center daily-margin">
+                                    <h4><strong>Wallet ID: chykeofficial</strong></h4>
+                                </div>
+                            </div>
+                        </div><div class="col-md-8 col-md-offset-2 daily-borderColor">
+                            <div class="row">
+                                <div class="col-md-12 text-center">
+                                    <h4><strong>Paid</strong></h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center daily-margin">
+                                    <h1><strong>N30,000</strong></h1>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center daily-margin">
+                                    <h4><strong>Wallet ID: chykeofficial</strong></h4>
                                 </div>
                             </div>
                         </div>
                     </div>
-            </div>
-
+                </div>
         </div>
+         
+        </div>
+<!-- Footer -->
+@include('footer')
+<!-- End footer-->
+</body>
     @endsection
+    
     
