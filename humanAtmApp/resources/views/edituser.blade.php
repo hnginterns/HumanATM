3b96883d2c267dc81898de92b858a6cc3192eadd
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-11-04 18:11:18 +0100

    fix merge conflict

--- humanAtmApp/resources/views/edituser.blade.php
+++ humanAtmApp/resources/views/edituser.blade.php
@@ -111,99 +111,6 @@
                             &nbsp;
                             &nbsp;                                     
 
-<<<<<<< HEAD
-                    <div class="col-md-10  col-md-offset-1">
-                        <form action="" id="update-details-form">
-                            <fieldset>
-                                <div class="row">
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label class="control-label text-muted" for="firstname">First Name*</label>
-                                        <input type="text" name="firstname" placeholder="First Name" class="form-control edituserpage-form " >
-                                    </div>
-
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label class="control-label text-muted" for="lastname">Last Name*</label>
-                                        <input type="text" name="lastname" placeholder="Last Name" class="form-control edituserpage-form">
-                                    </div>  
-                                &nbsp;
-
-                                    <div class="radio col-sm-4 text-center col-sm-offset-4">
-                                        <label class="text-muted">Gender</label><br><br>
-                                        <label><input type="radio" name="sports"> Male</label>
-                                        <label><input type="radio" name="sports"> Female</label>
-                                    </div>
-                                    <br>
-                                    &nbsp;
-                                    &nbsp;
-                                    &nbsp;
-                                    <br>
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label class="control-label text-muted" for="accountname">Account Name*</label>
-                                        <input type="text" name="accout_name" placeholder="Account Name" class="form-control edituserpage-form" >
-                                    </div>  
-                        
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label class="control-label text-muted" for="lastname">Account Number*</label>
-                                        <input type="number" name="account_number" placeholder="Account Name" class="form-control edituserpage-form" >
-                                   
-                                        <select class="withdraw-expand form-control"  name="bank_id">
-                                        <option value="" selected="selected">Select Bank</option>
-                                        
-                                        <option value=""></option>
-                                        
-                                    </select>
-                                    </div>  
-
-                                   
-
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label for="MobileNumber" class="text-muted control-label">Mobile Number*</label><br/>
-                                        <input type="tel" name="phone_number" placeholder="+2348112345678" class="form-control edituserpage-form" >
-                                    </div>
-                                    
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label for="EmailAddress" class="text-muted control-label">Email Address*</label><br/>
-                                        <input type="email" name="email" placeholder="user@humanatm.com" class="form-control edituserpage-form" >
-                                    </div>
-
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label class="control-label text-muted" for="location">Location</label>
-                                        <input type="text" name="location" placeholder="Location" class="form-control edituserpage-form" >
-                                    </div>  
-                                    <br/>
-                                    
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label for="Password" class="text-muted control-label"> OldPassword*</label><br/>
-                                        <input type="password" name="password" placeholder="***********" class="form-control edituserpage-form" >
-                                    </div>  
-
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label for="Password" class="text-muted control-label">Password*</label><br/>
-                                        <input type="password" name="password" placeholder="***********" class="form-control edituserpage-form" >
-                                    </div>                                         
-                                    <div class="col-md-8 col-md-offset-2 form-group">
-                                        <label for="Password" class="text-muted control-label">Re-enter Password*</label><br/>
-                                        <input type="password" name="password" placeholder="***********" class="form-control edituserpage-form" >
-                                    </div>  
-                                    <br>
-                                    &nbsp;
-                                    &nbsp;
-                                    &nbsp;                                     
-                                    <br>  
-                                  
-                                    <div class="col-xs-4 col-xs-offset-4 text-center">
-                                        <button type="submit" class="btn btn-success edit-btn">SAVE</button>
-                                    </div>
-                                    
-                                </div>                                    
-                            </fieldset>                            
-                        </form>                                   
-                    </div>                                                           
-                </div>           
-            </div>
-        </div>
-=======
-
                             <div class="col-xs-4 col-xs-offset-4 text-center">
                                 <button type="submit" class="btn btn-success edit-btn">SAVE</button>
                             </div>
@@ -213,7 +120,6 @@
                 </fieldset>                                  
             </div>                                                           
         </div>           
->>>>>>> 18418031c63ee62b96040875b77c18150ee90141
     </div>
 </div>
 </body>
