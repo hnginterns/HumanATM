598e5c95320e72bfca7af5de8b99a7f899ef0ce6
Mofope Ojosh <mofopet@gmail.com>
2017-11-09 00:56:03 +0100

    Route middleware group

--- humanAtmApp/routes/web.php
+++ humanAtmApp/routes/web.php
@@ -15,82 +15,77 @@ Route::get('/', function () {
 	return view('welcome');
 });
 
-
-Route::get('/location','TransactionController@index')->name('location');
-
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function () {
+
+	Route::get('/location','TransactionController@index')->name('location');
+
+	Route::get('/admin', 'AdminDashboardController@index')->name('admin');
+	Route::get('/admin/pending', 
+		'AdminDashboardController@pending')->name('admin/pending');
+	Route::get('/admin/humanAtm', 'AdminDashboardController@humanAtm')->name('admin/humanAtm');
+	Route::get('/admin/total', 
+		'AdminDashboardController@total')->name('admin/total');
+	Route::get('/admin/completed', 
+		'AdminDashboardController@completed')->name('admin/completed');
+		//--------------------------------------------
+		//INVITE
+		//---------------------------------------------
+	Route::post('/invite/send','InviteController@process');
 
 
-
-Route::get('/admin', 'AdminDashboardController@index')->name('admin');
-Route::get('/admin/pending', 
-	'AdminDashboardController@pending')->name('admin/pending');
-Route::get('/admin/humanAtm', 'AdminDashboardController@humanAtm')->name('admin/humanAtm');
-Route::get('/admin/total', 
-	'AdminDashboardController@total')->name('admin/total');
-Route::get('/admin/completed', 
-	'AdminDashboardController@completed')->name('admin/completed');
-	//--------------------------------------------
-	//INVITE
-	//---------------------------------------------
-Route::post('/invite/send','InviteController@process');
+	Route::get('/403', function () {
+		return view('errors.403');
+	})->name('403');
+	Route::get('/404', function () {
+		return view('errors.404');
+	})->name('404');
+	Route::get('/invite', 'InviteController@index');
 
 
-Route::get('/403', function () {
-	return view('errors.403');
-})->name('403');
-Route::get('/404', function () {
-	return view('errors.404');
-})->name('404');
- Route::get('/invite', 'InviteController@index');
+	/*  Transaction */
+
+	Route::get('/dashboard', 'UserDashboardController@index')->name('dashboard');
+	Route::get('/profile/{id}/update', 'ProfileController@create');
+
+	Route::post('/profile/update/{id}', 'ProfileController@store');
+	Route::get('/profile/image/{user_id}', 'ProfileController@showImageForm');
+	Route::post('/profile/image/{user_id}', 'ProfileController@updateImage');
 
 
-
-
-/*  Transaction */
-
-Route::get('/dashboard', 'UserDashboardController@index')->name('dashboard');
-Route::get('/profile/{id}/update', 'ProfileController@create')->middleware('auth');
-
-Route::post('/profile/update/{id}', 'ProfileController@store');
-Route::get('/profile/image/{user_id}', 'ProfileController@showImageForm')->middleware('auth');
-Route::post('/profile/image/{user_id}', 'ProfileController@updateImage');
-
-
-Route::get('/withdraw', function (){
-
-	return redirect('location');
-});
-Route::get('/human-atm/{id}', 'TransactionController@humanAtmProfile');
-Route::get('/withdraw/proceed/{human_atm_id}', 
-	'TransactionController@confirmProceed')->middleware('auth');
-Route::get('/process/withdraw/{human_atm_id}', 'TransactionController@processWithdraw');
-Route::post('/withdraw/', 'WalletsController@walletToAccount');
-Route::get('/withdraw/confirm/receipt/{withdrawal_id}', 'TransactionController@confirmWithdrawal');
-
-Route::get('/reject/payment/{payment_id}', 'TransactionController@rejectPayment');
-Route::get('/payment', 'TransactionController@showPaymentForm');
-Route::post('/payment', 'TransactionController@storePayment')->middleware('auth');
-
-Route::get('/fundwallet', 'WalletsController@showFundWallet');
-Route::post('/fund','WalletsController@cardToWallet');
-Route::post('/otp','WalletsController@otp');
-Route::get('wallet', 'WalletsController@walletBalance')->middleware('auth');
-
-
-Route::get('/{name}', function($name){
-
-     return view($name);
-});
-
-
-Route::get('/register/ref={code}', function($code){
-	
-		 return view('auth.register',compact('code'));
+	Route::get('/withdraw', function (){
+		return redirect('location');
 	});
+
+	Route::get('/human-atm/{id}', 'TransactionController@humanAtmProfile');
+	Route::get('/withdraw/proceed/{human_atm_id}', 'TransactionController@confirmProceed');
+	Route::get('/process/withdraw/{human_atm_id}', 'TransactionController@processWithdraw');
+	Route::post('/withdraw/', 'WalletsController@walletToAccount');
+	Route::get('/withdraw/confirm/receipt/{withdrawal_id}', 'TransactionController@confirmWithdrawal');
+
+	Route::get('/reject/payment/{payment_id}', 'TransactionController@rejectPayment');
+	Route::get('/payment', 'TransactionController@showPaymentForm');
+	Route::post('/payment', 'TransactionController@storePayment');
+
+	Route::get('/fundwallet', 'WalletsController@showFundWallet');
+	Route::post('/fund','WalletsController@cardToWallet');
+	Route::post('/otp','WalletsController@otp');
+	Route::get('wallet', 'WalletsController@walletBalance');
+});
+
+	
+
+	Route::get('/register/ref={code}', function($code){
+		
+			return view('auth.register',compact('code'));
+		});
+	Route::get('/{name}', function ($name){
+		return view($name);
+	});
+
 
 
 
